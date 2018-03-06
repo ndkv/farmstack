b1a7dd19fc9608e8641aa1ebda7d5c39f64be908
Simeon Nedkov <simeon@ndkv.nl>
2018-03-06 21:45:22 +0100

    enable bbox filter

--- farmstack/api/views.py
+++ farmstack/api/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from rest_framework import viewsets, mixins
+from django.contrib.gis.geos import Point, Polygon
 from serializers import Gewaspercelen2016Serializer, GewaspercelenGeoSerializer
 
 from models import Gewaspercelen2016
@@ -17,19 +18,19 @@ class Gewaspercelen2016ViewSet(mixins.ListModelMixin,
         x, y = point.split(',')
         return Gewaspercelen2016.objects.filter(geom__contains='POINT({} {})'.format(x, y))
 
-    # def bbox_filter(self, bbox):
-    #     bbox_geom = 'POLYGON (({}))'.format(bbox.replace(',', ' '))
-    #
-    #     return Gewaspercelen2016.objects.filter(geom__contained=bbox_geom)
+    def bbox_filter(self, bbox):
+        bbox_geom = Polygon.from_bbox(bbox.split(','))
+
+        return Gewaspercelen2016.objects.filter(geom__intersects=bbox_geom)
 
     def get_queryset(self):
         point = self.request.query_params.get('point', None)
         bbox = self.request.query_params.get('bbox', None)
 
-        if point is not None:
+        if point:
             return self.point_filter(point)
 
-        # if bbox is not None:
-        #     return self.bbox_filter(bbox)
+        if bbox:
+            return self.bbox_filter(bbox)
 
         return Gewaspercelen2016.objects.all()
